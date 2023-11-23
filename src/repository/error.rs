ad7c773035feedbca347252ca3b1291fe8c1bac5
David Skyberg <davidskyberg@gmail.com>
2023-11-23 09:46:41 -0500

    Removed DbError

--- src/repository/error.rs
+++
@@ -1,8 +0,0 @@
-#[derive(Debug, thiserror::Error)]
-pub enum DbError {
-    #[error("NotFound")]
-    NotFound,
-
-    #[error(transparent)]
-    DatabaseError(#[from] sqlx::Error),
-}
--- src/repository/mod.rs
+++ src/repository/mod.rs
@@ -1,6 +1,5 @@
 pub use postgres_url_repository::*;
 pub use url_repository::*;
 
-mod error;
 mod postgres_url_repository;
 mod url_repository;
